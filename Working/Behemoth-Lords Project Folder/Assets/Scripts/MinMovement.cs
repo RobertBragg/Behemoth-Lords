12b960164de6dad97757773301382e91a8d1d010
RobertBragg <Robert.Bragg@Ironplains.com>
2015-02-05 12:10:08 -0500

    Revert "Revert "Minion test comp Part 2""
    This reverts commit dbd0931f62190aa4d22acb0281e1c6d0f29b38f2.

---
+++ Working/Behemoth-Lords Project Folder/Assets/Scripts/MinMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum MoveType {ATTACK, FOLLOW, HOLD};
+
+public class MinMovement : MinNatural {
+	public int moveSpeed = 3;
+	public int rotationSpeed = 3;
+	private MoveType mType;
+	// Use this for initialization
+	void Start () {
+		mType = MoveType.HOLD;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform leadPos = leader.transform;
+		leadPos.position.Set(leadPos.position.x, transform.position.y, leadPos.position.z);
+
+		if(mType == MoveType.FOLLOW)
+		{
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(leadPos.position - transform.position), rotationSpeed*Time.deltaTime);
+
+			if(Vector3.Distance(leadPos.position, transform.position) > 4)
+			{
+				transform.position += transform.forward * moveSpeed * Time.deltaTime;
+			}
+		}
+		if (mType == MoveType.HOLD)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(leadPos.position - transform.position), rotationSpeed*Time.deltaTime);
+		}
+	}
+	
+	public void MoveTypeAttack()
+	{
+		Debug.Log("Attack");
+		mType = MoveType.ATTACK;
+	}
+
+	public void MoveTypeFollow()
+	{
+		Debug.Log("Follow");
+		mType = MoveType.FOLLOW;
+	}
+
+	public void MoveTypeHold()
+	{
+		Debug.Log("Hold");
+		mType = MoveType.HOLD;
+	}
+}
---
+++ Working/Behemoth-Lords Project Folder/Assets/Scripts/ShowCommandPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCommandPanel : MonoBehaviour {
+	GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		panel = GameObject.Find("CommandPanel");
+		panel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("CommandPanel"))
+		{
+			if(!panel.activeInHierarchy)
+				panel.SetActive(true);
+			else
+				panel.SetActive(false);
+
+		}
+	}
+}

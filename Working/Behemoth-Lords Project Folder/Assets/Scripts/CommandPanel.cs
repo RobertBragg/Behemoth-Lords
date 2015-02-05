dd3a8bed265ed2b2086cf42ce69ef05f545dcd3b
RobertBragg <Robert.Bragg@Ironplains.com>
2015-02-05 11:55:24 -0500

    Minion test comp Part 2
    continued commits

---
+++ Working/Behemoth-Lords Project Folder/Assets/Scripts/CommandPanel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandPanel : MonoBehaviour {
+	GameObject[] minions;
+	public AudioClip AudioAttackCommand;
+	public AudioClip AudioFollowCommand;
+	public AudioClip AudioHoldCommand;
+	// Use this for initialization
+	void Awake () {
+		minions = GameObject.FindGameObjectsWithTag("Minions");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("MinAttack"))
+		{
+			audio.PlayOneShot(AudioAttackCommand);
+			foreach(GameObject min in minions)
+			{
+				min.GetComponent<MinMovement>().MoveTypeAttack();
+			}
+		}
+		else if(Input.GetButtonDown("MinFollow"))
+		{			
+			audio.PlayOneShot(AudioFollowCommand);
+			foreach(GameObject min in minions)
+			{
+				min.GetComponent<MinMovement>().MoveTypeFollow();
+			}
+		}
+		else if(Input.GetButtonDown("MinHold"))
+		{			
+			audio.PlayOneShot(AudioHoldCommand);
+			foreach(GameObject min in minions)
+			{
+				min.GetComponent<MinMovement>().MoveTypeHold();
+			}
+		}
+	}
+}
---
+++ Working/Behemoth-Lords Project Folder/Assets/Scripts/MinNatural.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinNatural : MonoBehaviour {
+	protected GameObject leader;
+
+	void Awake(){
+		leader = GameObject.Find("First Person Controller");
+
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
